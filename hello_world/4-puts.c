466324d1b843e056909c53ec9b6cbe2fb91da59f
lchenevi <leo.cheneviere@gmail.com>
2023-10-13 01:52:29 -0700

    4-puts.c 2-fixed a few erros like adding a comment and deleted the new line

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-
-int main ()
+/* this is my commentary for the first C command*/
+int main (void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
