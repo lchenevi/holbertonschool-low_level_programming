0f07e6e828d5b618081f3d3e6baee5d3313b8a14
lchenevi <leo.cheneviere@gmail.com>
2023-10-16 06:48:33 -0700

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+	char y;
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	for (y = 'a'; y <= 'f'; y++)
+		putchar(y);
+	putchar('\n');
+	return (0);
+}
