ffdf2bd38effd7e032d6f07b5fcc4513302579d8
lchenevi <leo.cheneviere@gmail.com>
2023-10-16 06:31:50 -0700

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = '0'; x <= '9'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
