2859015b5a50c3a97e2118ab8c782863f2c0e044
lchenevi <leo.cheneviere@gmail.com>
2023-10-18 02:51:40 -0700

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - unction that adds two integers and returns the result.
+ * @x: input integer
+ * @y: input integer
+ *
+ * Return: x + y
+*/
+
+int add(int x, int y)
+{
+	return (x + y);
+}
