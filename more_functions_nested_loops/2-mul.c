c5dd431b285c0db03ec7651b7d040dccb2759515
lchenevi <leo.cheneviere@gmail.com>
2023-10-20 02:06:00 -0700

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *mul - multiply a and b
+ *@a: interger checked
+ *@b: interger checked
+ * Return: always 0
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
