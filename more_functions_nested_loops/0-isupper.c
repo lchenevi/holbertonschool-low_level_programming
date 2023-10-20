103f2580958c72cb4e41ab41d2997a61f9a7c353
lchenevi <leo.cheneviere@gmail.com>
2023-10-20 01:28:28 -0700

    0-isupper.c

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- Entry point
+ * Return: always 0
+ */
+int _isupper(int c)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
