65e69bf7a35494d26f083c62e99dca84430d7f45
lchenevi <leo.cheneviere@gmail.com>
2023-10-21 03:50:02 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *print_triangle - print_triangle
+ * @size: input number
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (j >= (size - i))
+			_putchar('#');
+			else
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+	if (size <= 0)
+	_putchar('\n');
+}
