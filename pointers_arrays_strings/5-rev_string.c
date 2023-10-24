a469536a41d0a167ae5f9bcf5585512edc18b450
lchenevi <leo.cheneviere@gmail.com>
2023-10-24 05:07:04 -0700

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *rev_string - displays a string reversed
+ *@s: checked
+ */
+void rev_string(char *s)
+{
+	int len = 0;
+	int i;
+	int j;
+
+    /*Find the length of the string*/
+    while (s[len] != '\0')
+        len++;
+
+    /*Reverse the string in-place*/
+    for (i = 0, j = len - 1; i < j; i++, j--)
+    {
+        char temp = s[i];
+        s[i] = s[j];
+        s[j] = temp;
+    }
+}
