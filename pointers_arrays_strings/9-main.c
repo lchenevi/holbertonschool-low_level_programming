f083cb05b29cd585dc0ee8f4950f43758198f79a
lchenevi <leo.cheneviere@gmail.com>
2023-10-24 07:29:28 -0700

    9-strcpy.c 2-light change

--- pointers_arrays_strings/9-main.c
+++ pointers_arrays_strings/9-main.c
@@ -1,11 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+/*#include <stdio.h>
 
-/**
+
  * main - check the code
  *
  * Return: Always 0.
- */
+ 
 int main(void)
 {
     char s1[98];
@@ -15,4 +15,29 @@ int main(void)
     printf("%s", s1);
     printf("%s", ptr);
     return (0);
+}*/
+#include <stdio.h>
+
+#define LEN 4
+
+void _memset(char *s, char c, int len);
+
+char *_strcpy(char *dest, char *src);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char cpy[LEN + 1] = {0};
+	char *str;
+	char *ret;
+
+	_memset(cpy, 'H', LEN);
+	str = "Z";
+	ret = _strcpy(cpy, str);
+	printf("%s\n%s\n%s\n", str, cpy, ret);
+	return (0);
 }
--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -13,7 +13,7 @@ char *_strcpy(char *dest, char *src)
 	int len;
 
 	len = 0;
-	while (src[len] != 0)
+	while (src[len] != '\0')
 	{
 		dest[len] = src[len];
 		len++;
