366b420f0b60b974418779e5539f3b6abc24caa9
lchenevi <leo.cheneviere@gmail.com>
2023-10-24 07:21:20 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string
+ * @dest: string destination
+ * @src: string source
+ *
+ * Return: sting destination
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int len;
+
+	len = 0;
+	while (src[len] != 0)
+	{
+		dest[len] = src[len];
+		len++;
+	}
+	return (dest);
+}
