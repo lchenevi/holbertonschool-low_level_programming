f0716243879019143690aa7a03a9b5aca5e701f0
lchenevi <leo.cheneviere@gmail.com>
2023-10-31 14:30:18 +0100

    task 6 betty

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,18 +10,17 @@ int is_prime_recursive(int n, int divisor)
 {
 	if (n <= 1)
 	{
-		return 0;/*1 and non-positive integers are not prime*/
+		return (0);/*1 and non-positive integers are not prime*/
 	}
 	if (divisor == 1)
 	{
-		return 1;/*Base case: n is prime*/
+		return (1);/*Base case: n is prime*/
 	}
 	if (n % divisor == 0)
 	{
-		return 0;
+		return (0);
 		/*n is divisible by a number other than 1, not prime*/
 	}
-
 	return is_prime_recursive(n, divisor - 1);
 }
 
@@ -34,9 +33,8 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 	{
-		return 0;
+		return (0);
 	}
-
 	/*Start the recursive function with divisor = n - 1*/
 	return is_prime_recursive(n, n - 1);
 }
