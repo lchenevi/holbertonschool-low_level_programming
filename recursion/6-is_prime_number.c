21be058cedd2a2e535e8e28e49bb880b8e94f119
lchenevi <leo.cheneviere@gmail.com>
2023-10-31 14:24:38 +0100

    task 6 without loops

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -1,26 +1,42 @@
 #include "main.h"
 
 /**
+ *is_prime_recursive - find the prime
+ *@n: checked
+ *@divisor: checked
+ *Return: n
+*/
+int is_prime_recursive(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return 0;/*1 and non-positive integers are not prime*/
+	}
+	if (divisor == 1)
+	{
+		return 1;/*Base case: n is prime*/
+	}
+	if (n % divisor == 0)
+	{
+		return 0;
+		/*n is divisible by a number other than 1, not prime*/
+	}
+
+	return is_prime_recursive(n, divisor - 1);
+}
+
+/**
  *is_prime_number - find the prime
  *@n: checked
  *Return: n
 */
 int is_prime_number(int n)
 {
-	int i;
-
-	if (n <= 1) /*1 and non-positive integers are not prime*/
+	if (n <= 1)
 	{
-		return (0);
-	}
-	for (i = 2; i * i <= n; i++)
-	{
-		if (n % i == 0)
-		/*If n is divisible by any number between 2 and sqrt(n)*/
-		{
-			return (0);
-		}
+		return 0;
 	}
 
-	return (1);/*If no divisors are found, it's prime*/
+	/*Start the recursive function with divisor = n - 1*/
+	return is_prime_recursive(n, n - 1);
 }
