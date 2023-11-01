4188ad1bf951488f94dc7f85f24e3d3ff06989b7
lchenevi <leo.cheneviere@gmail.com>
2023-11-01 16:52:47 +0100

    task 1

---
+++ argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints name program
+ * @argc: unused
+ * @argv: array argument
+ *
+ * Return: always 0
+ */
+int main (int argc, char *argv[])
+{
+	printf("%d\n", argv - 1);
+	return (0);
+}
