22681f93c21237b106c05ff6a75b8576684b8276
lchenevi <leo.cheneviere@gmail.com>
2023-11-01 17:00:14 +0100

    task 1

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - prints name program
- * @argc: unused
- * @argv: array argument
- *
- * Return: always 0
+ *main - prints name program
+ *@argc: unused
+ *@argv: array argument
+ *Return: always 0
  */
 int main (int argc, char *argv[] __attribute__((unused)))
 {
