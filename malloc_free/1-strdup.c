c3fcfd606e4855a3a8aa754fa0c42ad6390a2136
lchenevi <leo.cheneviere@gmail.com>
2023-11-06 15:59:14 +0100

    task 1

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ * _strdup - copies an array
+ * @str: checked
+ * Return: Returns the copy
+ */
+
+ char *_strdup(char *str)
+ {
+	char *duplicate;
+
+	if (str == NULL)
+		return (NULL);
+
+	duplicate = malloc(sizeof(duplicate) * sizeof(str) + 1);
+	if (duplicate == NULL)
+		return (NULL);
+	strcpy(duplicate, str);
+	return (duplicate);
+ }
