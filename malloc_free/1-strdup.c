43a461d613ac3500e4534891c06bd578757df5a7
lchenevi <leo.cheneviere@gmail.com>
2023-11-06 16:18:03 +0100

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -11,13 +11,27 @@
  char *_strdup(char *str)
  {
 	char *duplicate;
+	int length, i;
 
+	/*Check if str is NULL*/
 	if (str == NULL)
 		return (NULL);
 
-	duplicate = malloc(sizeof(duplicate) * sizeof(str) + 1);
+	/*Calculate the length of the input string*/
+	length = 0;
+	while (str[length] != '\0')
+		length++;
+
+	/*Allocate memory for the duplicate string*/
+	duplicate = malloc(sizeof(duplicate) * sizeof(length) + 1);
+
+	/*Check if memory allocation failed*/
 	if (duplicate == NULL)
 		return (NULL);
-	strcpy (duplicate, str);
+
+	/*Manually copy the characters from str to duplicate*/
+	for (i = 0; i <= length; i++)
+		duplicate[i] = str[i];
+
 	return (duplicate);
  }
