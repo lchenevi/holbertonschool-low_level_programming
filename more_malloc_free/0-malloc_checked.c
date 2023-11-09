a3977804778e5a97abc043202a377def619d6af1
lchenevi <leo.cheneviere@gmail.com>
2023-11-09 11:05:31 +0100

    task 0

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * malloc_checked - creates array of chars
+ * @b: checked
+ * Return: Returns a pointer to the array, or NULL if it fails
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit (98);
+	return (ptr);
+}
