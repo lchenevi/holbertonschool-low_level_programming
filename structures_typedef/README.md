e87ddbbe92006370e9579ca382bab9e76a166d48
lchenevi <leo.cheneviere@gmail.com>
2023-11-14 11:08:36 +0100

    task 0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+this my readme for this project
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - header
+* @name: checked
+* @age: checked
+* @owner: checked
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
