12c58bf437492dd88d1cc198e588cba6f1710b67
lchenevi <leo.cheneviere@gmail.com>
2023-12-05 16:38:29 +0100

    task 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * create_file - Create a function that creates a file.
+ *
+ * @filename: name of file
+ * @text_content: the text to print on file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content == NULL)
+		text_content = "";
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+
+	if (write(fd, text_content, strlen(text_content)) == -1)
+	{
+		close(fd);
+		return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
